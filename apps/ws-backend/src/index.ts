4bfef829ee6f4686dc349c7e156968d15803701f
Your Name <adarshmauryajay@gmail.com>
2026-01-13 11:25:03 +0530

    gated the ws server using the token

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,23 @@
 import { WebSocketServer } from 'ws';
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from './config';
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws,request) {
+  const url=request.url;
+  if(!url){
+    return;
+  }
+  const tokenurl=new URLSearchParams(url.split('?')[1]);
+  const token=tokenurl.get("token") ?? "";
+  const decoded=jwt.verify(token,JWT_SECRET)
+
+  //@ts-ignore
+  if(!decoded || !decoded.userId){
+    ws.close();
+    return;
+  }
 
   ws.on('message', function message(data) {
   ws.send('ping');
