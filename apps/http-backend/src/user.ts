86906c87d50cde031caae6e768d3812c175e8caa
Your Name <adarshmauryajay@gmail.com>
2026-01-16 11:46:03 +0530

    Removed db package

--- apps/http-backend/src/user.ts
+++ apps/http-backend/src/user.ts
@@ -4,21 +4,43 @@ import { JWT_SECRET } from "@repo/back-common/config";
 import {createuserschema} from "@repo/common/types";
 import {signinschema} from "@repo/common/types";
 import {createroomschema} from "@repo/common/types";
-import prismaClient from "@repo/db/client";
 
 
 const signup=async(req:Request,res:Response)=>{
-    //dbcall to create user
-    const data = createuserschema.safeParse(req.body);
-    if(!data){
-        res.status(403).json({
-            message:"Invalid Inputs",
+    try {
+        //dbcall to create user
+        const input = createuserschema.safeParse(req.body);
+        
+        if(!input){
+            res.status(403).json({
+                message:"Invalid Inputs",
+            })
+            return ;
+        }
+        // const user= await prisma.user.create({
+        //     data:{
+        //         //@ts-ignore
+        //         name:input.data?.name,
+        //         //@ts-ignore
+        //         email:input.data?.username,
+        //         //@ts-ignore
+        //         password:input.data?.password
+        //     }
+        // })
+        // if(!user){
+        //     return res.status(400).json({
+        //         message:"user not registered",
+        //     })
+        // }
+        return res.status(200).json({
+            message:"User signup completed",
         })
-        return ;
+    } catch (error) {
+        return res.status(400).json({
+            message:"Error in user signup",
+            error:error
+        })
     }
-    res.json({
-        userId:1
-    })
 }
 
 const signin=async(req:Request,res:Response)=>{
